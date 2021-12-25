fe3c73134ec9f471e4c5d889193e30208a345458
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-12-25 18:42:41 -0500

    Angular material

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesHomeComponent } from './pages/heroes-home/heroes-home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { BuscarComponent } from './pages/bsucar/buscar.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarComponent,
+    HeroeComponent,
+    BuscarComponent,
+    HeroesHomeComponent,
+    ListadoComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
