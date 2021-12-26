d3cebf43c96548fbb7ea3807c09d167bd345f142
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-12-25 21:59:47 -0500

    Listado de heroes

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  id!: string;
+  constructor(private ruta: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.ruta.params.subscribe(({ id }) => this.id = id);
+    console.log(this.id);
   }
 
 }
